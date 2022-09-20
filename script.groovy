93e98c6b34b24cf261dda268715a24d99345e0c5
GhazouaniHichem <ghazouanih68@gmail.com>
2022-09-20 12:47:12 +0100

    FIx bug

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJAR() {
 
 def buildImage() {
     echo "========Building the docker image========"
-    withCredentials([usernamePassword(credentialsId: 'docker-hu-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ghazouanihm/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push ghazouanihm/demo-app:jma-2.0'
